f4bec3ceb8757a64354d2bb24fcf5dd5dc1dcd53
Embbnux Ji <embbnux@embbnux.com>
2024-07-11 16:57:42 +0800

    feat: use Embeddable 2.0 (#89)

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ function createMainWindow() {
   mainWindow.setBrowserView(mainView);
   mainView.setBounds({ x: 0, y: 37, width: 300, height: 500 });
   mainView.setAutoResize({ width: true, height: true });
-  let appUrl = 'https://apps.ringcentral.com/integration/ringcentral-embeddable/latest/app.html';
+  let appUrl = 'https://apps.ringcentral.com/integration/ringcentral-embeddable/2.x/app.html';
   appUrl = `${appUrl}?appVersion=${version}&userAgent=RingCentralEmbeddableForLinux/${version}&enableRingtoneSettings=1&enableNoiseReductionSetting=1`;
   if (rcClientId) {
     appUrl = `${appUrl}&clientId=${rcClientId}`;
